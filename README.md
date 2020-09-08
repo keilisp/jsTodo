6621060cef8ebf101f29798449447fbb571f8356
mediocre <druksasha@ukr.net>
2020-09-08 19:05:07 +0300

    added readme

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-todo-app
+# Todo app
+
+# Description
+
+Simple todo app built with vanilla JavaScript
+
+# Installation
+
+Clone the repo into your folder
+
+```sh
+cd *your-folder*
+git clone https://github.com/mediocreeee/jsTodo.git
+```
+
+Then just open _index.html_ file in your browser
--- script.js
+++ script.js
@@ -1,152 +1,140 @@
-///////////////////////////////////////////////////////////////////////////////
-//// TODO: LOCAL STORAGE REALIZATION
-//// TODO: setItem() &&  getItem()
-//// TODO: GIT REPOSITORY PUSH
-///////////////////////////////////////////////////////////////////////////////
+// TODO: add local storage
+// TODO: api to get and set items
 
-
-///////////////////////////////////////////////////////////////////////////////
-let inputElement = document.getElementById('input');
-let ulElem = document.getElementById('list');
-let actionPanel1 = document.getElementById('actionPanel1');
-let actionPanel2 = document.getElementById('actionPanel2');
+// Init items
+let inputElement = document.getElementById("input");
+let ulElem = document.getElementById("list");
+let actionPanel1 = document.getElementById("actionPanel1");
+let actionPanel2 = document.getElementById("actionPanel2");
 let todoList = [];
-///////////////////////////////////////////////////////////////////////////////
 
+actionPanel2.style.display = "none";
 
-///////////////////////////////////////////////////////////////////////////////
-actionPanel2.style.display = 'none'
-///////////////////////////////////////////////////////////////////////////////
-
-
-///////////////////////////////////////////////////////////////////////////////
-inputElement.addEventListener('keydown', event => {
-  if ((event.key == 'Enter' || event.keyCode == 13) && (inputElement.value !== '')) {
+// Listen for entering new todo
+inputElement.addEventListener("keydown", (event) => {
+  if (
+    (event.key == "Enter" || event.keyCode == 13) &&
+    inputElement.value !== ""
+  ) {
     todoList.unshift({
       content: inputElement.value,
-      done: false
+      done: false,
     });
     console.log(todoList);
-    inputElement.value = '';
+    inputElement.value = "";
 
-    upgradeView()
+    upgradeView();
   }
 });
-///////////////////////////////////////////////////////////////////////////////
 
-
-///////////////////////////////////////////////////////////////////////////////
+// Update function
 function upgradeView() {
-
   for (let i = 0; i < todoList.length; i++) {
-    localStorage.getItem (todoList[i].content, todoList[i].done)
+    localStorage.getItem(todoList[i].content, todoList[i].done);
   }
 
-  ulElem.innerHTML = ''
+  ulElem.innerHTML = "";
 
   for (let i = 0; i < todoList.length; i++) {
-    const todoItem = todoList[i]
+    const todoItem = todoList[i];
 
-    let liElem = document.createElement('li')
-    let divElem = document.createElement('div')
-    let checkboxElem = document.createElement('input')
-    let labelElem = document.createElement('label')
-    let buttonDoneElem = document.createElement('button')
-    let buttonRemElem = document.createElement('button')
+    let liElem = document.createElement("li");
+    let divElem = document.createElement("div");
+    let checkboxElem = document.createElement("input");
+    let labelElem = document.createElement("label");
+    let buttonDoneElem = document.createElement("button");
+    let buttonRemElem = document.createElement("button");
 
-    liElem.className = 'list-group-item';
+    liElem.className = "list-group-item";
     ulElem.append(liElem);
 
-    divElem.className = 'form-group form-check item'
-    liElem.append(divElem)
+    divElem.className = "form-group form-check item";
+    liElem.append(divElem);
 
-    divElem.append(checkboxElem)
-    checkboxElem.type = 'checkbox'
-    checkboxElem.className = 'form-check-input'
-    checkboxElem.id = 'todoItem' + i
-    checkboxElem.checked = todoItem.selected
-    checkboxElem.addEventListener('change', () => {
-      todoItem.selected = checkboxElem.checked
+    divElem.append(checkboxElem);
+    checkboxElem.type = "checkbox";
+    checkboxElem.className = "form-check-input";
+    checkboxElem.id = "todoItem" + i;
+    checkboxElem.checked = todoItem.selected;
+    checkboxElem.addEventListener("change", () => {
+      todoItem.selected = checkboxElem.checked;
       upgradeView();
-    })
+    });
 
-    divElem.append(labelElem)
-    labelElem.className = 'form-check-label'
+    divElem.append(labelElem);
+    labelElem.className = "form-check-label";
     if (todoItem.done) {
-      labelElem.className += " todoDone"
+      labelElem.className += " todoDone";
     }
-    labelElem.setAttribute('for', 'todoItem' + i)
-    labelElem.innerText = todoItem.content
+    labelElem.setAttribute("for", "todoItem" + i);
+    labelElem.innerText = todoItem.content;
 
     if (todoItem.done == false) {
-      divElem.append(buttonDoneElem)
-      buttonDoneElem.className = 'btn btn-outline-primary'
-      buttonDoneElem.type = 'button'
-      buttonDoneElem.innerText = 'Done'
-      buttonDoneElem.style = 'float: right'
+      divElem.append(buttonDoneElem);
+      buttonDoneElem.className = "btn btn-outline-primary";
+      buttonDoneElem.type = "button";
+      buttonDoneElem.innerText = "Done";
+      buttonDoneElem.style = "float: right";
 
-      buttonDoneElem.addEventListener('click', () => {
-        todoItem.done = !todoItem.done
-        upgradeView()
-
-      })
-    } else {
-      divElem.append(buttonRemElem)
-      buttonRemElem.className = 'btn btn-outline-danger'
-      buttonRemElem.type = 'button'
-      buttonRemElem.innerText = 'Remove'
-      buttonRemElem.style = 'float: right'
-
-      buttonRemElem.addEventListener('click', () => {
-        todoList = todoList.filter(currentTodoItem => currentTodoItem !== todoItem)
+      buttonDoneElem.addEventListener("click", () => {
+        todoItem.done = !todoItem.done;
         upgradeView();
-      })
+      });
+    } else {
+      divElem.append(buttonRemElem);
+      buttonRemElem.className = "btn btn-outline-danger";
+      buttonRemElem.type = "button";
+      buttonRemElem.innerText = "Remove";
+      buttonRemElem.style = "float: right";
+
+      buttonRemElem.addEventListener("click", () => {
+        todoList = todoList.filter(
+          (currentTodoItem) => currentTodoItem !== todoItem
+        );
+        upgradeView();
+      });
     }
   }
-  let someSelected = todoList.some(todoItem => todoItem.selected)
+  let someSelected = todoList.some((todoItem) => todoItem.selected);
   if (someSelected) {
-    actionPanel2.style.display = 'block'
-  }else {
-    actionPanel2.style.display = 'none'
+    actionPanel2.style.display = "block";
+  } else {
+    actionPanel2.style.display = "none";
   }
-  // for (let i = 0; i < todoList.length; i++) {
-  //   localStorage.setItem(todoList[i].content, todoList[i].done)
-  // }
 }
-///////////////////////////////////////////////////////////////////////////////
 
-
-
-///////////////////////////////////////////////////////////////////////////////
-document.getElementById('doneAction').addEventListener('click', () => {
+// Done function
+document.getElementById("doneAction").addEventListener("click", () => {
   for (let todoItem of todoList) {
     if (todoItem.selected) {
-      todoItem.done = true
-      todoItem.selected = false
+      todoItem.done = true;
+      todoItem.selected = false;
     }
   }
   upgradeView();
-})
+});
 
-document.getElementById('restoreAction').addEventListener('click', () => {
+// Restore todo function
+document.getElementById("restoreAction").addEventListener("click", () => {
   for (let todoItem of todoList) {
     if (todoItem.selected) {
-      todoItem.done = false
-      todoItem.selected = false
+      todoItem.done = false;
+      todoItem.selected = false;
     }
   }
   upgradeView();
-})
+});
 
-document.getElementById('removeAction').addEventListener('click', () => {
-  todoList = todoList.filter(todoItem => !todoItem.selected)
+// Remove todo function
+document.getElementById("removeAction").addEventListener("click", () => {
+  todoList = todoList.filter((todoItem) => !todoItem.selected);
   upgradeView();
-})
+});
 
-document.getElementById("selectAll").addEventListener('click', () => {
+// Select all todos function
+document.getElementById("selectAll").addEventListener("click", () => {
   for (let todoItem of todoList) {
-    todoItem.selected = true
+    todoItem.selected = true;
   }
   upgradeView();
-})
-///////////////////////////////////////////////////////////////////////////////
+});
